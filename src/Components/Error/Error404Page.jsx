9e04ee61a1f2b7934bb887de885f4b0e057c34f6
Arjit <arjitloh223@gmail.com>
2024-08-06 18:56:55 +0545

    Error page and routs

---
+++ src/Components/Error/Error404Page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Error404Page = () => {
+  return (
+    <div>
+<h5>
+        Error 404: 
+</h5>
+        <h6>Page not found</h6>
+    </div>
+  )
+}
+
+export default Error404Page
--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Authentication from '../Pages/Authentication'
+import Dashboard from '../Pages/Dashboard'
+import Error404Page from '../Components/Error/Error404Page'
 
 const MainRoutes = () => {
   return (
     <Routes>
-
+        <Route path='' element={<Authentication/>} />
+        <Route path='/Dashboard' element={<Dashboard/>} />
+        <Route path='*' element={<Error404Page/>} />
     </Routes>
   )
 }
